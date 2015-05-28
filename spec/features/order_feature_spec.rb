ed0b7e502207d6265057b1d7f185d9c9fc6ccd13
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-28 10:03:06 -0600

    better spec

--- spec/features/order_feature_spec.rb
+++ spec/features/order_feature_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.feature "order feature" do
   scenario "a user can order an item" do
     visit "items/1"
-    expect(page).to have_content("Sleek Wooden Pants")
+    expect(page).to have_content("Add To Order")
     click_link_or_button "Add To Order"
     expect(page).to have_content("Information About Your Order")
+    expect(page).to have_content("#{User.first.name}")
   end
 end
