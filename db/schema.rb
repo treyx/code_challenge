e7ff2c6cb5da1a35141060ecc9943f8457b508a3
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-28 09:41:32 -0600

    added item_id to order table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216235605) do
+ActiveRecord::Schema.define(version: 20150528153957) do
 
   create_table "items", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20131216235605) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "item_id"
   end
 
   create_table "users", force: true do |t|
